38f55842be9003cb6bcfb6dc18fbcb3567fbb9cf
“ijasz” <jass07rtr@gmail.com>
2023-04-21 18:26:21 +0530

    nested if & list

---
+++ List/index.py
@@ -0,0 +1,5 @@
+a = [1, 2, 3, 4, 5, 6, 1]
+
+print(a)
+a.append(7)
+print(a[-1])
---
+++ conditional-statement/nested-if.py
@@ -0,0 +1,49 @@
+# a = "hello"
+
+# print(a.count("l"))
+
+# num = 10
+
+# if (num % 5 == 0 and num % 11 == 0):
+#     print("it is divisible by 5 & 11")
+# else:
+#     print("it is not divisible by 5 & 11")
+
+
+a = 50
+b = 20
+c = 30
+
+# if (a > b and a > c):
+#     print("a is greater")
+# elif (b > c):
+#     print("b is greater")
+# else:
+#     print("c is greater")
+
+# if (a < b and a < c):
+#     print("a is smaller")
+# elif (b < c):
+#     print("b is smaller")
+# else:
+#     print("c is smaller")
+
+
+# if (a > b and a > c):
+#     print("a is greater")
+#     if (b < c):
+#         print("b is smaller")
+#     else:
+#         print("c is smaller")
+# elif (b > c):
+#     print("b is greater")
+#     if (a < c):
+#         print("a is smaller")
+#     else:
+#         print("c is smaller")
+# else:
+#     print("c is greater")
+#     if (a < c):
+#         print("a is smaller")
+#     else:
+#         print("c is smaller")
