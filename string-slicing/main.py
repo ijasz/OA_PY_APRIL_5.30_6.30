67ec7ec2f837704b7b9b740aac2208f08434bf2c
“ijasz” <jass07rtr@gmail.com>
2023-05-09 18:33:12 +0530

    string, list slicing

---
+++ string-slicing/main.py
@@ -0,0 +1,21 @@
+a = "abcdefghijklmnopqrstuvwxyz"
+# l = [1, 2, 3, 4, 5]
+
+# print(l[0:2])
+
+# print(a[-2::-2])
+
+# print(a[-4:5:-1])
+
+# a[start:stop:step]
+
+# print(a[0:5:1])
+# print(a[0:5])
+# print(a[:5])
+
+
+# print(a[0::2])
+# print(a[::2])
+
+# print(a[-1::-1])
+# print(a[::-1])
