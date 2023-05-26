e2dd8f532e1a12fde920a487bda4a4e4068b9e10
“ijasz” <jass07rtr@gmail.com>
2023-05-26 18:41:48 +0530

    main

--- fileHandling/main.py
+++ fileHandling/main.py
@@ -1,8 +1,36 @@
+import os
+
 f = open("demo.txt", 'r')
 
-print(f.read(2))
+# print(f.read(2))
 
-print(type(f.readlines()))
+# print(f.readlines())
+
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
 
 # for i in f.readline():
 #     print(i)
+
+# f.close()
+
+# f1 = open("demo2.txt", 'a')
+# f1 = open("demo.txt", 'a')
+# open("demo3.txt", 'x')
+
+# for i in range(1, 4):
+#     os.remove(f"demo{i}.py")
+
+# f1.write("Now the file has more content!")
+# f.close()
+
+# if os.path.exists("demofile.txt"):
+#     os.remove("demofile.txt")
+# else:
+#     print("The file does not exist")
+
+# try:
+#     os.rmdir("folder")
+# except:
+#     print("some went wrong")
--- task_function/task3.py
+++ task_function/task3.py
@@ -1,8 +1,2 @@
-def fun(n):
-    n = 5
-    i = 0
-    for i in range(1, n+1):
-        n += i
-
-
-fun(n)
+def armstrong():
+    number = int(input("Enter the no:"))
