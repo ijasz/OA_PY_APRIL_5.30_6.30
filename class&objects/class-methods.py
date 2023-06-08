b91ae0b6c70c46df2df1bd583712c3cd2b72f42c
“ijasz” <jass07rtr@gmail.com>
2023-06-08 18:57:23 +0530

    class-methods.py

--- class&objects/class-methods.py
+++ class&objects/class-methods.py
@@ -6,9 +6,9 @@ class Person:
 
     def getDetails(self):
         print("FirstName : ", self.firstname)
-        print("LastName : ", self.lastname)
-        print("Age : ", self.age)
-        print("Location : ", self.location)
+        print("LastName  : ", self.lastname)
+        print("Age       : ", self.age)
+        print("Location  : ", self.location)
 
     def getFullname(self):
         print("Fullname : ",  self.firstname + " " + self.lastname)
@@ -20,17 +20,17 @@ class Person:
         if(args["location"]):
                 self.location = args["location"]
                 print("Location : ", self.location)
-        # print("LastName : ", self.lastname)
-        # print("Age : ", self.age)
-        # print("Location : ", self.location)
+        # print("LastName       : ", self.lastname)
+        # print("Age            : ", self.age)
+        # print("Location       : ", self.location)
 
 
 obj1 = Person()
 
 obj1.firstname = "ocean"
-obj1.lastname = "academy"
-obj1.age = 10
-obj1.location = "pondy"
+obj1.lastname  = "academy"
+obj1.age       = 25
+obj1.location  = "pondy"
 
 
 obj1.getDetails()
