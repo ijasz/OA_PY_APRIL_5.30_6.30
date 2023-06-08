b31f4e611d0a5859faef56aa25be26eb831ae866
“ijasz” <jass07rtr@gmail.com>
2023-06-08 18:26:10 +0530

    class-methods.py

---
+++ class&objects/class-methods.py
@@ -0,0 +1,39 @@
+class Person:
+    firstname = "---"
+    lastname = "---"
+    age = 0
+    location = "---"
+
+    def getDetails(self):
+        print("FirstName : ", self.firstname)
+        print("LastName : ", self.lastname)
+        print("Age : ", self.age)
+        print("Location : ", self.location)
+
+    def getFullname(self):
+        print("Fullname : ",  self.firstname + " " + self.lastname)
+
+    def updateDetails(self, **args):
+        # if(args["firstname"]):
+        #     self.firstname = args["firstname"]
+        #     print("FirstName : ", self.firstname)
+        if(args["location"]):
+                self.location = args["location"]
+                print("Location : ", self.location)
+        # print("LastName : ", self.lastname)
+        # print("Age : ", self.age)
+        # print("Location : ", self.location)
+
+
+obj1 = Person()
+
+obj1.firstname = "ocean"
+obj1.lastname = "academy"
+obj1.age = 10
+obj1.location = "pondy"
+
+
+obj1.getDetails()
+
+obj1.updateDetails(location = "chennai")
+# obj1.getFullname()
